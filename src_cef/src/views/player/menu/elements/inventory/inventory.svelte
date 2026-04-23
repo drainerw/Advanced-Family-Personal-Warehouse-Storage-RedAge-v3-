f5b466c920a9bc67d53832c825da29cc4001c2d2
drainerw <letuzhq@gmail.com>
2026-04-23 15:06:35 +0300

    FIX

--- src_cef/src/views/player/menu/elements/inventory/inventory.svelte
+++ src_cef/src/views/player/menu/elements/inventory/inventory.svelte
@@ -2,7 +2,11 @@
 
 <!-- 1. Ищем условие отрисовки прогресс-бара инвентаря ("Вес", "Вместимость") -->
 <!-- Обычно это около 2166 строки. Ищем: -->
-OtherInfo.Id === otherType.Storage
+{#if OtherInfo.Id === otherType.Organization || OtherInfo.Id === otherType.Fraction || OtherInfo.Id === otherType.Vehicle
 
-<!-- ЗАМЕНЯЕМ НА: -->
+<!-- ДОБАВЛЯЕМ: -->
 (OtherInfo.Id === otherType.Storage || OtherInfo.Id === otherType.PublicWarehouse)
+
+
+<!-- ПОЛУЧАЕТСЯ ТАК : -->
+{#if OtherInfo.Id === otherType.Organization || OtherInfo.Id === otherType.Fraction || OtherInfo.Id === otherType.Vehicle || OtherInfo.Id === otherType.Safe || OtherInfo.Id === otherType.Storage || OtherInfo.Id === otherType.PublicWarehouse}
